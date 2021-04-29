de64cb17b4dd86fec7672feddac01c0883d25307
Valgreen <alvaro.valverde@alumnos.uach.cl>
2021-04-29 16:10:29 -0400

    prueba decode

--- proyecto/mycodec.py
+++ proyecto/mycodec.py
@@ -17,20 +17,62 @@ def denoise(frame):
     
     frame = np.real(fftpack.ifft2(fftpack.ifftshift(espectro_filtrado)))
     
-    return frame
+    #filtro sal y pimienta
+    img_filtered = medfilt(frame, 5)
+           
+    return img_filtered
 
-def code(frame):
+def code(img_filtered):
     #
     # Implementa en esta función el bloque transmisor: Transformación + Cuantización + Codificación de fuente
-    #    
-    return frame
+    #
+    # Se recorre la imagen en bloques de 8x8
+    imsize = img_filtered.shape
+    colores_matrix = np.zeros(shape=imsize)
+    for i in range(0, imsize[0], 8):
+       for j in range(0, imsize[1], 8):
+       
+          colores_matrix[i:(i+8),j:(j+8)] = img_filtered[i:(i+8),j:(j+8)]
+    
+    colores_matrix = colores_matrix.astype(int)
+    m = 5
+    new_colores = np.array([x-x%m for x in colores_matrix])
+    colores_compressed = []
+    strcolores_compressed = ""
 
+    actual = new_colores[0, 0]
+    anterior = actual
+    for i in range(imsize[0]):
+    
+       contador = 0
+    
+       for j in range(imsize[1]):
+        
+           actual = new_colores[i, j]
+        
+           if (new_colores[i, j] == anterior):
+            
+              contador += 1
+        
+           else:
+            
+              colores_compressed.append(contador)
+              colores_compressed.append(anterior)
+              strcolores_compressed += str(contador) + "!" + str(anterior) + "!"
+              anterior = actual
+              
+    return strcolores_compressed
 
 def decode(message):
     #
     # Reemplaza la linea 24...
     #
-    frame = np.frombuffer(bytes(memoryview(message)), dtype='uint8').reshape(480, 848)
+    message_decode = message.split("!")
+    lista = [message_decode[i:i+64] for i in range(0,len(message_decode),64)]
+    lista_bloques=[]
+    for i in lista:
+       lista_bloques.append([i[j:j+8] for j in range(0,len(i),8)])
+    #frame = np.frombuffer(bytes(memoryview(message)), dtype='uint8').reshape(480, 848)
     #
     # ...con tu implementación del bloque receptor: decodificador + transformación inversa
     #    
